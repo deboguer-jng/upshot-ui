409ec9bc767a0c68e00cb71333f56b3adb1c5312
Ryan Bell <25379378+iRyanBell@users.noreply.github.com>
2021-08-13 17:37:39 -0700

    Docs

--- src/themes/theme-ui.d.ts
+++ src/themes/theme-ui.d.ts
@@ -3,6 +3,10 @@ import UpshotUITheme from './UpshotUI'
 
 type UpshotUIThemeType = typeof UpshotUITheme
 
+/**
+ * Provides theme types for styled components.
+ * @see https://emotion.sh/docs/typescript#define-a-theme
+ */
 declare module '@emotion/react' {
   export interface Theme extends UpshotUIThemeType {}
 }
