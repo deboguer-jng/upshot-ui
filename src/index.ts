b8e8799d8094f23722a3e0860ee84dd74548a21e
Ryan Bell <25379378+iRyanBell@users.noreply.github.com>
2021-08-17 14:25:08 -0700

    Export components

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,17 @@
+/* Components */
+export { default as Avatar } from './components/Avatar'
+export { default as AvatarInitials } from './components/AvatarInitials'
+export { default as Checkbox } from './components/Checkbox'
+export { default as Container } from './components/Container'
 export { default as Icon } from './components/Icon'
+export { default as IconButton } from './components/IconButton'
+export { default as InputRounded } from './components/InputRounded'
+export { default as InputRoundedSearch } from './components/InputRoundedSearch'
+export { default as Pagination } from './components/Pagination'
+export { default as Panel } from './components/Panel'
+export { default as Radio } from './components/Radio'
+export { default as Switch } from './components/Switch'
 
+/* Theme */
 export { default as UpshotThemeProvider, globalStyles } from './themes'
 export { default as theme, UpshotUIThemeType } from './themes/UpshotUI'
