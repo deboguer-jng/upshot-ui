eec2245ab768fb838cabd07740c344996acffac8
Ethan Bennett <ebennett19@gmail.com>
2021-08-17 01:50:31 -0600

    Fix alignment for percentage variant; refactor

--- src/components/LabelAttribute/Styled.tsx
+++ src/components/LabelAttribute/Styled.tsx
@@ -17,27 +17,24 @@ export const AttributeLabelBase = styled.div<AttributeLabelBaseProps>`
   width: 282px;
   display: inline-block;
   background-color: ${({ $transparent }) => $transparent ? 'transparent' : colors.transparentBlue };
-`
-
-export const LabelText = styled.div`
   font-family: ${ fonts.body };
   font-weight: ${ fontWeights.body };
   font-size: ${ fontSizes[4] };
-  padding-left: ${ sizes[3] + 'px' };
   padding-top: ${ sizes[1] + 'px' };
+`
+
+export const RightAlignBlock = styled.div`
+  float: right;
+  padding-right: ${ sizes[3] + 'px' };
+`
+
+export const LabelText = styled.div`
+  padding-left: ${ sizes[3] + 'px' };
   display: inline-block;
-  `
+`
 
-  export const RightAlignBlock = styled.div`
-    float: right;
-    padding-right: ${ sizes[3] + 'px' };
-  `
-
-  export const Division = styled.div`
-    padding-right: ${ sizes[3] + 'px' };
-    display: inline-block;
-    font-size: ${ fontSizes[5] };
-    vertical-align: middle;
-    margin-top: -3px;
-    font-weight: 200;
-  `
+export const Division = styled.div`
+  font-size: ${ fontSizes[4] };
+  padding-right: ${ sizes[3] + 'px' };
+  display: inline-block;
+`
