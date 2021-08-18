69af0f0bc57c704d97499c2f4328f005399b8426
DaWe <willteam35@gmail.com>
2021-08-18 13:39:27 +0200

    SVG spinner

--- src/components/Spinner/Styled.tsx
+++ src/components/Spinner/Styled.tsx
@@ -1,67 +1,34 @@
 import styled from '@emotion/styled'
-import colors from '../../themes/UpshotUI/colors'
+import { SpinnerSize } from './'
 
 interface StyledSpinnerProps {
-  $size: number
+  $size: SpinnerSize
 }
 
-export const StyledSpinner = styled.div`
-  font-size: 10px;
-  margin: 50px auto;
-  text-indent: -9999em;
-  width: ${(props: StyledSpinnerProps) => props.$size}px;
-  height: ${(props: StyledSpinnerProps) => props.$size}px;
-  border-radius: 50%;
-  background: #ffffff;
-  background: linear-gradient(to right, #fff 0%, rgba(255, 255, 255, 0) 80%);
-  position: relative;
-  -webkit-animation: load3 1.4s infinite linear;
-  animation: load3 1.4s infinite linear;
-  -webkit-transform: translateZ(0);
-  -ms-transform: translateZ(0);
-  transform: translateZ(0);
-  &:before {
-    width: 100%;
-    height: 50%;
-    background: #ffffff;
-    background: linear-gradient(to right, rgba(255, 255, 255, 0) 0%, #fff 80%);
-    border-radius: ${(props: StyledSpinnerProps) => props.$size/2}px ${(props: StyledSpinnerProps) => props.$size/2}px 0 0;
-    position: absolute;
-    top: 0;
-    left: 0;
-    content: '';
-  }
-  &:after {
-    background: ${({ theme }) => theme.colors.background};
-    width: 70%;
-    height: 70%;
-    border-radius: 50%;
-    content: '';
-    margin: auto;
-    position: absolute;
-    top: 0;
-    left: 0;
-    bottom: 0;
-    right: 0;
-  }
-  @-webkit-keyframes load3 {
-    0% {
-      -webkit-transform: rotate(0deg);
-      transform: rotate(0deg);
-    }
+export const StyledSpinner = styled.div<StyledSpinnerProps>`
+  width: ${({ theme, $size }) => theme.spinners.sizes[$size]}px;
+  height: ${({ theme, $size }) => theme.spinners.sizes[$size]}px;
+  transform: rotate(0deg);
+
+  /* Continuously rotate the spinner. */
+  animation: spin 1.4s infinite linear;
+  @keyframes spin {
     100% {
-      -webkit-transform: rotate(360deg);
       transform: rotate(360deg);
     }
   }
-  @keyframes load3 {
-    0% {
-      -webkit-transform: rotate(0deg);
-      transform: rotate(0deg);
-    }
-    100% {
-      -webkit-transform: rotate(360deg);
-      transform: rotate(360deg);
-    }
-  }
+`
+
+export const StyledGradient = styled.div`
+  width: 100%;
+  height: 100%;
+  border-radius: ${({ theme }) => theme.radii.circle};
+
+  background-image: conic-gradient(
+    from 90deg at 50% 50%,
+    #fff0 0deg,
+    #fff 345deg,
+    #fff 0deg,
+    #fff0 0deg
+  );
 `
--- src/themes/UpshotUI/spinners.ts
+++ src/themes/UpshotUI/spinners.ts
@@ -1,13 +1,13 @@
-import { fontSizes } from './text'
+import sizes, { getSize } from './sizes'
 
 const spinners = {
   sizes: {
-    xs: 16,
-    sm: 32,
-    md: 58,
-    lg: 94,
-    xl: 188,
-  }
+    xs: getSize(4),
+    sm: getSize(8),
+    md: getSize(16),
+    lg: getSize(24),
+    xl: getSize(48),
+  },
 }
 
 export default spinners
