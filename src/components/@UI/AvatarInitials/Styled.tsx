7d9845d4f33a1caa06672099d27c6d669e03628d
Ryan Bell <25379378+iRyanBell@users.noreply.github.com>
2021-08-24 17:57:48 -0700

    Improve tables / avatars

--- src/components/@UI/AvatarInitials/Styled.tsx
+++ src/components/@UI/AvatarInitials/Styled.tsx
@@ -2,15 +2,23 @@ import styled from '@emotion/styled'
 import { Box } from 'theme-ui'
 import { AvatarSize } from '../Avatar'
 
-export const AvatarInitialsBase = styled(Box)`
+export const AvatarInitialsBase = styled(Box)<{ $size: AvatarSize }>`
   display: inline-flex;
+  border: none;
   border-radius: ${({ theme }) => theme.radii.circle};
   background: ${({ theme }) => theme.gradients.primary};
+  padding: ${({ theme, $size }) => theme.images.avatar[$size].borderWidth};
   text-transform: uppercase;
 `
 
 export const AvatarInitialsShadow = styled.div<{ $size: AvatarSize }>`
+  display: flex;
+  flex-grow: 1;
+  align-items: center;
+  justify-content: center;
+  font-family: ${({ theme }) => theme.fonts.body};
+  line-height: 1;
   border-radius: ${({ theme }) => theme.radii.circle};
-  margin: ${({ theme, $size }) => theme.images.avatar[$size].borderWidth};
   box-shadow: 0 0 16px rgba(0, 0, 0, 0.3);
+  user-select: none;
 `
