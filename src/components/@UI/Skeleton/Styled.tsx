2946ac7154ce0611bf432288086b5def31ef930f
DaWe <willteam35@gmail.com>
2021-08-27 11:41:51 +0200

    Remove moving bar and add fade effect

--- src/components/@UI/Skeleton/Styled.tsx
+++ src/components/@UI/Skeleton/Styled.tsx
@@ -1,10 +1,12 @@
 import styled from '@emotion/styled'
 import { keyframes } from '@emotion/react'
 import { Box } from 'theme-ui'
+import colors from '../../../themes/UpshotUI/colors'
 
 const loadingFlash = keyframes`
-  0% { background-position: -250px; }
-  100% { background-position: calc(100% + 250px); }
+  25%  {background-color: ${ colors['grey-700'] };}
+  50%  {background-color: ${ colors['grey-600'] };}
+  75% {background-color: ${ colors['grey-700'] };}
 `
 
 interface StyledSkeletonProps {
@@ -19,26 +21,5 @@ export const StyledSkeleton = styled(Box)<StyledSkeletonProps>`
   min-width: ${ minWidth };
   min-height: ${ minHeight };
   border-radius: ${({ theme, $circle }) => ($circle === true ? theme.radii.circle : theme.radii.md)};
-  &::before {
-    min-width: ${ minWidth };
-    min-height: ${ minHeight };
-    content: '';
-    display: block;
-    width: 100%;
-    height: 100%;
-    border-radius: ${({ theme, $circle }) => ($circle === true ? theme.radii.circle : theme.radii.md)};
-    background: ${({ theme }) => `
-      linear-gradient(
-        90deg,
-        ${theme.colors['grey-700']} 0px,
-        ${theme.colors['grey-600']} 30%,
-        ${theme.colors['grey-600']} 70%,
-        ${theme.colors['grey-700']} 100%
-      )
-    `};
-    background-size: 35%;
-    background-position: 0%;
-    background-repeat: no-repeat;
-    animation: ${loadingFlash} 1.2s infinite linear;
-  }
+  animation: ${loadingFlash} 1.2s infinite linear;
 `
