38bb65c24c26d989ad384fe6309ff2866993c64d
Ryan Bell <25379378+iRyanBell@users.noreply.github.com>
2021-09-02 19:17:48 -0700

    Improve interface

--- src/components/@UI/MiniNftCard/index.tsx
+++ src/components/@UI/MiniNftCard/index.tsx
@@ -15,105 +15,107 @@ import {
 } from './Styled'
 
 export interface MiniNftCardInterface {
+  price: string
+  rarity: string
   creator?: string
   image?: string
   error?: boolean
   name?: string
-  price?: number
-  rarity?: number
   from?: string
   to?: string
   date?: string
-  type: 'default' | 'search'
+  type?: 'default' | 'search'
 }
 
 const MiniNftCard = forwardRef(
-  ({
-    error = false,
-    type = 'default',
-    creator,
-    image,
-    name,
-    price,
-    rarity,
-    from,
-    to,
-    date,
-    ...props
-  }: MiniNftCardInterface,
-  ref: React.ForwardedRef<HTMLDivElement>
+  (
+    {
+      error = false,
+      type = 'default',
+      creator,
+      image,
+      name,
+      price,
+      rarity,
+      from,
+      to,
+      date,
+      ...props
+    }: MiniNftCardInterface,
+    ref: React.ForwardedRef<HTMLDivElement>
   ) => {
-  return (
-    <MiniNftCardWrapper {...{ ref, ...props }}>
-      <MiniNftCardMainBoard error={error}>
-        <img src={error ? ErrorSvg : image} />
-        <MiniNftCardMainContentWrapper type={type}>
+    return (
+      <MiniNftCardWrapper {...{ ref, ...props }}>
+        <MiniNftCardMainBoard error={error}>
+          <img src={error ? ErrorSvg : image} />
+          <MiniNftCardMainContentWrapper type={type}>
+            {type === 'default' ? (
+              <MiniNftCardPrice error={error}>
+                {error ? 'Error' : price}
+              </MiniNftCardPrice>
+            ) : (
+              <MiniNftCardName error={error}>
+                {error ? 'Error' : name}
+              </MiniNftCardName>
+            )}
+          </MiniNftCardMainContentWrapper>
+        </MiniNftCardMainBoard>
+        <MiniNftCardDetailsBoard>
+          {type === 'search' ? (
+            <MiniNftCardDetailLabel variant="xSmall">
+              Creator:
+            </MiniNftCardDetailLabel>
+          ) : null}
+          <MiniNftCardDetailsName variant="small" error={error}>
+            {error ? 'Error' : type === 'default' ? name : creator}
+          </MiniNftCardDetailsName>
           {type === 'default' ? (
-            <MiniNftCardPrice error={error}>
-              {error ? 'Error' : `$${price.toFixed(2)}`}
-            </MiniNftCardPrice>
-          ) : (
-            <MiniNftCardName error={error}>
-              {error ? 'Error' : name}
-            </MiniNftCardName>
-          )}
-        </MiniNftCardMainContentWrapper>
-      </MiniNftCardMainBoard>
-      <MiniNftCardDetailsBoard>
-        {type === 'search' ? (
+            <MiniNftCardDetailValue variant="xSmall" error={error}>
+              {error ? 'Error' : date}
+            </MiniNftCardDetailValue>
+          ) : null}
           <MiniNftCardDetailLabel variant="xSmall">
-            Creator:
+            {type === 'default' ? 'From :' : 'Rarity :'}
           </MiniNftCardDetailLabel>
-        ) : null}
-        <MiniNftCardDetailsName variant="small" error={error}>
-          {error ? 'Error' : type === 'default' ? name : creator}
-        </MiniNftCardDetailsName>
-        {type === 'default' ? (
-          <MiniNftCardDetailValue variant="xSmall" error={error}>
-            {error ? 'Error' : date}
+          <MiniNftCardDetailValue variant="small" error={error}>
+            {error ? (
+              type === 'default' ? (
+                'Error'
+              ) : (
+                '--%'
+              )
+            ) : type === 'default' ? (
+              <Flex sx={{ alignItems: 'center' }}>
+                <AddressCircle variant="from" />
+                <Text variant="small"> {from} </Text>
+              </Flex>
+            ) : (
+              rarity
+            )}
           </MiniNftCardDetailValue>
-        ) : null}
-        <MiniNftCardDetailLabel variant="xSmall">
-          {type === 'default' ? 'From :' : 'Rarity :'}
-        </MiniNftCardDetailLabel>
-        <MiniNftCardDetailValue variant="small" error={error}>
-          {error ? (
-            type === 'default' ? (
-              'Error'
+          <MiniNftCardDetailLabel variant="xSmall">
+            {type === 'default' ? 'To :' : 'Price :'}
+          </MiniNftCardDetailLabel>
+          <MiniNftCardDetailValue variant="small" error={error}>
+            {error ? (
+              type === 'default' ? (
+                'Error'
+              ) : (
+                '$--'
+              )
+            ) : type === 'default' ? (
+              <Flex sx={{ alignItems: 'center' }}>
+                <AddressCircle variant="to" />
+                <Text variant="small"> {to} </Text>
+              </Flex>
             ) : (
-              '--%'
-            )
-          ) : type === 'default' ? (
-            <Flex sx={{ alignItems: 'center' }}>
-              <AddressCircle variant="from" />
-              <Text variant="small"> {from} </Text>
-            </Flex>
-          ) : (
-            `${rarity.toFixed(2)}%`
-          )}
-        </MiniNftCardDetailValue>
-        <MiniNftCardDetailLabel variant="xSmall">
-          {type === 'default' ? 'To :' : 'Price :'}
-        </MiniNftCardDetailLabel>
-        <MiniNftCardDetailValue variant="small" error={error}>
-          {error ? (
-            type === 'default' ? (
-              'Error'
-            ) : (
-              '$--'
-            )
-          ) : type === 'default' ? (
-            <Flex sx={{ alignItems: 'center' }}>
-              <AddressCircle variant="to" />
-              <Text variant="small"> {to} </Text>
-            </Flex>
-          ) : (
-            `$${price}`
-          )}
-        </MiniNftCardDetailValue>
-      </MiniNftCardDetailsBoard>
-    </MiniNftCardWrapper>
-  )
-})
+              price
+            )}
+          </MiniNftCardDetailValue>
+        </MiniNftCardDetailsBoard>
+      </MiniNftCardWrapper>
+    )
+  }
+)
 
 export default MiniNftCard
