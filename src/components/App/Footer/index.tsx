053da8c42d4613deafc9676b49a295d512ac45c8
devbiran <ib660987@gmail.com>
2021-09-13 22:55:10 +0200

    footer mobile responsiveness added.

--- src/components/App/Footer/index.tsx
+++ src/components/App/Footer/index.tsx
@@ -1,10 +1,17 @@
 import React, { forwardRef } from 'react'
-import { FooterLine, StyledBoxMiddleSeparate, StyledBox, StyledBoxLeft, StyledBoxRight, StyledBoxMiddle } from './Styled'
+import {
+  FooterLine,
+  StyledBoxMiddleSeparate,
+  StyledBox,
+  StyledBoxLeft,
+  StyledBoxRight,
+  StyledBoxMiddle,
+  LogoBox,
+} from './Styled'
 import Flex from '../../Layout/Flex'
 import { FlexProps, Link } from 'theme-ui'
 import Icon from '../../@UI/Icon'
 import IconButton from '../../@UI/IconButton'
-
 
 /**
  * Footer at the bottom of the application.
@@ -12,84 +19,53 @@ import IconButton from '../../@UI/IconButton'
 const Footer = forwardRef(
   ({ ...props }: FlexProps, ref: React.ForwardedRef<HTMLDivElement>) => {
     return (
-      <FooterLine gap={2} columns={[3, '2fr,1fr,2fr']} { ...props } >
-
+      <FooterLine {...props}>
         <StyledBoxLeft>
-          Upshot
+          <LogoBox>Upshot</LogoBox>
+          <StyledBox>
+            <Flex>
+              <StyledBoxMiddle>Whitepaper</StyledBoxMiddle>
+              <StyledBoxMiddleSeparate>|</StyledBoxMiddleSeparate>
+              <StyledBoxMiddle>Privacy</StyledBoxMiddle>
+              <StyledBoxMiddleSeparate>|</StyledBoxMiddleSeparate>
+              <StyledBoxMiddle>FAQ</StyledBoxMiddle>
+            </Flex>
+          </StyledBox>
         </StyledBoxLeft>
-        <StyledBox>
-          <Flex>
-              <StyledBoxMiddle>              
-                  Whitepaper               
-              </StyledBoxMiddle>
-              <StyledBoxMiddleSeparate>|</StyledBoxMiddleSeparate>
-              <StyledBoxMiddle>               
-                  Privacy               
-              </StyledBoxMiddle>
-              <StyledBoxMiddleSeparate>|</StyledBoxMiddleSeparate>
-              <StyledBoxMiddle>          
-                  FAQ               
-              </StyledBoxMiddle>
-          </Flex>
-        </StyledBox>
         <StyledBoxRight>
-        <Flex>
-              <StyledBoxMiddle>
-
+          <Flex>
+            <StyledBoxMiddle>
               <Link href="https://blog.upshot.io/" target="_blank">
-                <IconButton>  
-                  <Icon
-                    color="grey-700"
-                    icon="mediumBlock"
-                    size={24}
-                  />
+                <IconButton>
+                  <Icon color="grey-700" icon="mediumBlock" size={24} />
                 </IconButton>
               </Link>
-                
-              </StyledBoxMiddle>
-              <StyledBoxMiddle>
-                
+            </StyledBoxMiddle>
+            <StyledBoxMiddle>
               <Link href="https://twitter.com/upshothq" target="_blank">
-                <IconButton>  
-                  <Icon
-                    color="grey-700"
-                    icon="twitterCircle"
-                    size={24}
-                  />
+                <IconButton>
+                  <Icon color="grey-700" icon="twitterCircle" size={24} />
                 </IconButton>
               </Link>
-                
-              </StyledBoxMiddle>
-              <StyledBoxMiddle>
-              
-                <Link href="https://discord.gg/upshot" target="_blank">
-                  <IconButton>  
-                    <Icon
-                      color="grey-700"
-                      icon="discord"
-                      size={24}
-                    />
-                  </IconButton>
-                </Link>
-              
-              </StyledBoxMiddle>
-              <StyledBoxMiddle>
-                
+            </StyledBoxMiddle>
+            <StyledBoxMiddle>
+              <Link href="https://discord.gg/upshot" target="_blank">
+                <IconButton>
+                  <Icon color="grey-700" icon="discord" size={24} />
+                </IconButton>
+              </Link>
+            </StyledBoxMiddle>
+            <StyledBoxMiddle>
               <Link href="https://www.instagram.com/upshot.hq/" target="_blank">
-                <IconButton>  
-                  <Icon
-                    color="grey-700"
-                    icon="instagramCircle"
-                    size={24}
-                  />
+                <IconButton>
+                  <Icon color="grey-700" icon="instagramCircle" size={24} />
                 </IconButton>
               </Link>
-                
-              </StyledBoxMiddle>
+            </StyledBoxMiddle>
           </Flex>
         </StyledBoxRight>
       </FooterLine>
-    ) 
+    )
   }
 )
 
