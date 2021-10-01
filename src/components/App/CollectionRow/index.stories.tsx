b8973b19783a13c17d7a716598aebf34d113cd13
Ethan Bennett <ebennett19@gmail.com>
2021-10-01 01:44:17 -0400

    http => https

--- src/components/App/CollectionRow/index.stories.tsx
+++ src/components/App/CollectionRow/index.stories.tsx
@@ -83,7 +83,7 @@ Default.args = {
       title:
         'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum sodales quis ex nec sagittis.',
       imageSrc:
-        'http://res.cloudinary.com/upshot-inc/image/upload/v1628114947/xriprlqfkheg6dzhl6j7.png',
+        'https://res.cloudinary.com/upshot-inc/image/upload/v1628114947/xriprlqfkheg6dzhl6j7.png',
       columns: ['Value 1', 'Value 2', 'Value 3'],
       pixelated: true,
     },
