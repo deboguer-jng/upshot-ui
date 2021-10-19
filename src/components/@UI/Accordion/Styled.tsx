20f656b0a028b0ce1c085868b4310799d7dd3616
devbiran <ib660987@gmail.com>
2021-10-19 18:07:59 +0200

    search filter dropdown transition & z-index implemented.

--- src/components/@UI/Accordion/Styled.tsx
+++ src/components/@UI/Accordion/Styled.tsx
@@ -2,21 +2,32 @@ import styled from '@emotion/styled'
 import theme from '../../../themes/UpshotUI'
 
 interface AccordionHeaderProps {
+  isDropdown?: boolean
   open: boolean
 }
 
 interface AccordionBodyProps {
+  isDropdown?: boolean
+  open: boolean
+}
+
+interface AccordionBodyWrapperProps {
+  isDropdown?: boolean
   open: boolean
 }
 
 export const AccordionWrapper = styled.div`
   position: relative;
   margin-bottom: ${({ theme }) => theme.accordion.marginBottom}px;
+  min-height: 54px;
 `
 
 export const AccordionHeader = styled.div<AccordionHeaderProps>`
   position: relative;
-  z-index: ${({ theme }) => theme.zIndex.default + 2};
+  z-index: ${({ theme, isDropdown, open }) =>
+    open
+      ? (isDropdown ? theme.zIndex.dropdown : theme.zIndex.default) + 4
+      : (isDropdown ? theme.zIndex.dropdown : theme.zIndex.default) + 2};
   border-radius: ${({ theme }) => theme.accordion.header.borderRadius}px;
   width: 100%;
   padding: ${({ theme }) =>
@@ -26,31 +37,43 @@ export const AccordionHeader = styled.div<AccordionHeaderProps>`
   display: flex;
   align-items: center;
   justify-content: space-between;
+  transition: ${({ theme, open }) => !open && theme.transitions.default};
+  transition-delay: ${({ open }) => !open && '0.15s'};
 
   svg {
     height: 16px;
     width: 16px;
 
     transform: rotate(${({ open }) => (open ? '180deg' : '0deg')});
-
+    transition: ${({ theme }) => theme.transitions.default};
     path {
       fill: ${({ theme }) => theme.colors.primary};
     }
   }
 `
 
-export const AccordionBody = styled.div<AccordionBodyProps>`
-  position: relative;
+export const AccordionBodyWrapper = styled.div<AccordionBodyWrapperProps>`
+  position: ${({ isDropdown }) => (isDropdown ? 'absolute' : 'relative')};
+  z-index: ${({ theme, isDropdown, open }) =>
+    open
+      ? (isDropdown ? theme.zIndex.dropdown : theme.zIndex.default) + 3
+      : (isDropdown ? theme.zIndex.dropdown : theme.zIndex.default) + 1};
   width: 100%;
-  z-index: ${({ theme }) => theme.zIndex.default + 1};
   top: 0;
   left: 0;
-  margin-top: -50px;
-  overflow: hidden;
+  margin-top: ${({ isDropdown }) => (isDropdown ? '27px' : '-27px')};
   background-color: ${({ theme }) => theme.colors['grey-900']};
   border-radius: ${({ theme }) => theme.accordion.body.borderRadius}px;
+  transition: ${({ theme, open }) => !open && theme.transitions.default};
+  transition-delay: ${({ open }) => !open && '0.15s'};
+`
+
+export const AccordionBody = styled.div<AccordionBodyProps>`
   padding: ${({ theme }) => theme.accordion.body.padding}px;
-  padding-top: 50px;
+  padding-top: ${({ open }) => (open ? '27px' : 0)};
   padding-bottom: ${({ open }) => (open ? theme.accordion.body.padding : 0)}px;
-  height: ${({ open }) => (open ? 'fit-content' : '0px')};
+  max-height: ${({ open }) => (open ? '1000px' : '0px')};
+  overflow: hidden;
+  transition: ${({ theme }) => theme.transitions.default};
+  ${({ theme: { scroll } }) => scroll.thin}
 `
--- src/components/@UI/Accordion/index.tsx
+++ src/components/@UI/Accordion/index.tsx
@@ -1,33 +1,65 @@
-import React, { forwardRef, ReactNode, useState } from 'react'
-import { AccordionWrapper, AccordionHeader, AccordionBody } from './Styled'
+import React, {
+  forwardRef,
+  ReactNode,
+  useState,
+  useEffect,
+  useRef,
+} from 'react'
+import {
+  AccordionWrapper,
+  AccordionHeader,
+  AccordionBody,
+  AccordionBodyWrapper,
+} from './Styled'
 import Icon from '../Icon'
 import Text from '../Text'
 
 export interface AccordionProps {
   title: string
   children: ReactNode
+  isDropdown?: boolean
 }
 
 const Accordion = forwardRef(
   (
-    { title, children, ...props }: AccordionProps,
+    { title, children, isDropdown, ...props }: AccordionProps,
     ref: React.ForwardedRef<HTMLDivElement>
   ) => {
     const [accodionOpen, setAccordionOpen] = useState(false)
+    const dropdownRef = useRef<HTMLDivElement>()
+
+    useEffect(() => {
+      const handleClickOutside = (e: MouseEvent) => {
+        if (!dropdownRef.current.contains(e.target as Node) && isDropdown) {
+          setAccordionOpen(false)
+        }
+      }
+
+      document.addEventListener('mousedown', handleClickOutside)
+
+      return () => document.removeEventListener('mousedown', handleClickOutside)
+    }, [])
 
     return (
-      <AccordionWrapper {...{ ref, ...props }}>
-        <AccordionHeader
-          open={accodionOpen}
-          onClick={() => setAccordionOpen(!accodionOpen)}
-        >
-          <Text color="text" variant="large">
-            {title}
-          </Text>
-          <Icon icon="arrowDropdown" />
-        </AccordionHeader>
-        <AccordionBody open={accodionOpen}>{children}</AccordionBody>
-      </AccordionWrapper>
+      <div {...{ ref, ...props }}>
+        <AccordionWrapper ref={dropdownRef}>
+          <AccordionHeader
+            open={accodionOpen}
+            isDropdown={isDropdown}
+            onClick={() => setAccordionOpen(!accodionOpen)}
+          >
+            <Text color="text" variant="large">
+              {title}
+            </Text>
+            <Icon icon="arrowDropdown" />
+          </AccordionHeader>
+          <AccordionBodyWrapper isDropdown={isDropdown} open={accodionOpen}>
+            <AccordionBody isDropdown={isDropdown} open={accodionOpen}>
+              {children}
+            </AccordionBody>
+          </AccordionBodyWrapper>
+        </AccordionWrapper>
+      </div>
     )
   }
 )
