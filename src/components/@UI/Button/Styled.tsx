6f60a223899c3e524b32f537055043e574ee4fb2
devbiran <ib660987@gmail.com>
2021-11-04 16:37:16 +0100

    buttom component capitalize param added.

--- src/components/@UI/Button/Styled.tsx
+++ src/components/@UI/Button/Styled.tsx
@@ -8,10 +8,12 @@ interface PrimaryButtonProps {
   toggled: boolean
   width: number
   minimized: boolean
+  capitalize?: boolean
 }
 
 interface PlainButtonProps {
   $size: keyof typeof Buttons.property
+  capitalize?: boolean
 }
 
 export const PrimaryButton = styled.button<PrimaryButtonProps>`
@@ -39,7 +41,7 @@ export const PrimaryButton = styled.button<PrimaryButtonProps>`
   justify-content: center;
   padding: 0px 25px;
   transition: ${({ theme }) => theme.transitions.default};
-  text-transform: uppercase;
+  text-transform: ${({ capitalize }) => capitalize ? 'capitalize' : 'uppercase'};
 
   & svg {
     margin-right: ${({ theme, $size, minimized }) =>
@@ -59,6 +61,7 @@ export const PrimaryButton = styled.button<PrimaryButtonProps>`
   span {
     flex-grow: 1;
     font-family: ${({ theme }) => theme.fonts.body};
+    text-transform: ${({ capitalize }) => capitalize ? 'capitalize' : 'uppercase'};
     color: ${({ theme, $type, toggled }) =>
       typeof toggled === 'undefined'
         ? theme.buttons.variants[$type].colors.color
@@ -181,6 +184,10 @@ export const PlainButton = styled.button<PlainButtonProps>`
   font-family: ${({ theme }) => theme.fonts.body};
   cursor: pointer;
   height: ${({ theme, $size }) => theme.buttons.property[$size].height}px;
+
+  span {
+    text-transform: ${({ capitalize }) => capitalize ? 'capitalize' : 'uppercase'};
+  }
 
   &:not(:disabled):hover {
     span {
