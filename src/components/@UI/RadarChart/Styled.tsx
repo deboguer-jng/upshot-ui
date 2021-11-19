fe1199f40276576a7213c00d5d918a234e509b7d
Ryan Bell <25379378+iRyanBell@users.noreply.github.com>
2021-11-18 16:15:53 -0800

    Radar charts

---
+++ src/components/@UI/RadarChart/Styled.tsx
@@ -0,0 +1,10 @@
+import styled from '@emotion/styled'
+
+export const RadarChartWrapper = styled.div`
+  width: 100%;
+
+  & .apexcharts-radar-series line,
+  & .apexcharts-radar-series polygon {
+    stroke: rgba(255, 255, 255, 0.25);
+  }
+`
