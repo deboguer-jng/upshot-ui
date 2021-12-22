a674e2948f233eba77752cdd97c5740c170c4a69
DaWe <willteam35@gmail.com>
2021-12-22 13:46:47 +0100

    Merged Box and div into one element so sx will work as expected

--- src/components/@UI/SwitchDropdown/index.tsx
+++ src/components/@UI/SwitchDropdown/index.tsx
@@ -8,7 +8,6 @@ import {
 } from './Styled'
 import Text from '../Text'
 import Icon from '../Icon'
-import Box from '../../Layout/Box'
 
 export interface SwitchDropdownProps extends BoxProps {
   value: string
@@ -31,38 +30,36 @@ const SwitchDropdown = forwardRef(
     }
 
     return (
-      <Box>
-        <SwitchDropdownWrapper {...{ ref, ...props }}>
-          <SwitchDropdownHeader
-            open={open}
-            onClick={() => {
-              setOpen(!open)
-              onToggle && onToggle(!open)
-            }}
-          >
-            <Text variant="h1Secondary" color="primary">
-              {value ? value : options[0]}
-            </Text>
-            <Icon icon="arrowDropdown" />
-          </SwitchDropdownHeader>
-          {open && (
-            <SwitchDropdownOptions open={open}>
-              {options
-                .filter((option, index) => (value ? option !== value : index))
-                .map((option, index) => (
-                  <SwitchDropdownOption
-                    key={index}
-                    onClick={() => handleClick(option)}
-                  >
-                    <Text variant="h2Secondary" color="primary">
-                      {option}
-                    </Text>
-                  </SwitchDropdownOption>
-                ))}
-            </SwitchDropdownOptions>
-          )}
-        </SwitchDropdownWrapper>
-      </Box>
+      <SwitchDropdownWrapper {...{ ref, ...props }}>
+        <SwitchDropdownHeader
+          open={open}
+          onClick={() => {
+            setOpen(!open)
+            onToggle && onToggle(!open)
+          }}
+        >
+          <Text variant="h1Secondary" color="primary">
+            {value ? value : options[0]}
+          </Text>
+          <Icon icon="arrowDropdown" />
+        </SwitchDropdownHeader>
+        {open && (
+          <SwitchDropdownOptions open={open}>
+            {options
+              .filter((option, index) => (value ? option !== value : index))
+              .map((option, index) => (
+                <SwitchDropdownOption
+                  key={index}
+                  onClick={() => handleClick(option)}
+                >
+                  <Text variant="h2Secondary" color="primary">
+                    {option}
+                  </Text>
+                </SwitchDropdownOption>
+              ))}
+          </SwitchDropdownOptions>
+        )}
+      </SwitchDropdownWrapper>
     )
   }
 )
