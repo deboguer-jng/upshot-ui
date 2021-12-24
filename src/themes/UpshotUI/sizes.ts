56639ae0ff03c1fc6c7854f659468e524c46637e
Ryan Bell <25379378+iRyanBell@users.noreply.github.com>
2021-12-24 15:28:18 -0800

    Updated bkps

--- src/themes/UpshotUI/sizes.ts
+++ src/themes/UpshotUI/sizes.ts
@@ -5,12 +5,13 @@ export const getSize = (n: number) => n * SIZE_UNIT
 const sizes = [...new Array(16)].map((_, n) => getSize(n))
 
 export const breakpointsNamed = {
+  xxs: '320px', // Custom lower-bound
   xs: '640px',
   sm: '768px',
   md: '1024px',
   lg: '1280px',
   xl: '1536px',
-  xxl: '1678px',
+  xxl: '1678px', // Custom upper-bound
 }
 
 export const breakpoints = Object.values(breakpointsNamed)
