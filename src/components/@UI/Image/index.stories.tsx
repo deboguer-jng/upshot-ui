038749973ce28729bd6876148434c6be79f6c9ea
DaWe <willteam35@gmail.com>
2021-12-30 13:17:37 +0100

    add http image to storybook

--- src/components/@UI/Image/index.stories.tsx
+++ src/components/@UI/Image/index.stories.tsx
@@ -14,6 +14,11 @@ Default.args = {
   src: 'https://res.cloudinary.com/upshot-inc/image/upload/v1631819750/uvwr9fujso4awqpjxlfq.png',
 }
 
+export const DefaultHttp = Template.bind({})
+DefaultHttp.args = {
+  src: 'http://res.cloudinary.com/upshot-inc/image/upload/v1631819750/uvwr9fujso4awqpjxlfq.png',
+}
+
 export const W100 = Template.bind({})
 W100.args = {
   src: 'https://res.cloudinary.com/upshot-inc/image/upload/v1631819750/uvwr9fujso4awqpjxlfq.png',
