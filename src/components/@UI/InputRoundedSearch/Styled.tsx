4b963af1c7f02aa04b641dfd07c5e4bef732f725
Ryan Bell <25379378+iRyanBell@users.noreply.github.com>
2021-12-31 10:50:08 +0200

    Improve search suggestion box

--- src/components/@UI/InputRoundedSearch/Styled.tsx
+++ src/components/@UI/InputRoundedSearch/Styled.tsx
@@ -23,7 +23,7 @@ export const InputRoundedSearchBase = styled(
 
 export const InputRoundedSearchWrapper = styled.div<InputRoundedSearchWrapperProps>`
   position: relative;
-  width: ${({ $isMobile }) => $isMobile ? '100%' : 'fit-content' };
+  width: ${({ $isMobile }) => ($isMobile ? '100%' : 'fit-content')};
 `
 
 export const InputRoundedSearchSuggestionsWrapper = styled.div`
@@ -31,9 +31,11 @@ export const InputRoundedSearchSuggestionsWrapper = styled.div`
   top: 0;
   left: 0;
   border-radius: 20px;
-  padding-top: 52px;
-  width: 100%;
+  margin-top: 54px;
+  width: calc(100% + 24px);
+  margin-left: -12px;
   background-color: rgba(0, 0, 0, 0.8);
+  border: 1px solid ${({ theme }) => theme.colors['grey-500']};
   backdrop-filter: blur(4px);
   overflow: hidden;
   z-index: ${({ theme }) => theme.zIndex.default + 1};
@@ -42,11 +44,12 @@ export const InputRoundedSearchSuggestionsWrapper = styled.div`
 export const InputRounededSearchSuggestions = styled.div`
   overflow: auto;
   max-height: 150px;
+  margin: 8px;
   ${({ theme: { scroll } }) => scroll.thin}
 `
 
 export const InputRoundedSearchSuggestionItem = styled.div`
-  padding: 5px 16px;
+  padding: 8px 16px;
   cursor: pointer;
   transition: all 0.1s linear;
 
