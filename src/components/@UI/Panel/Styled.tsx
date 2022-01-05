5b1628adbcf9f08f1d64c8bdabb64f268413f992
Ryan Bell <25379378+iRyanBell@users.noreply.github.com>
2022-01-05 09:25:00 +0200

    Increase panel padding

--- src/components/@UI/Panel/Styled.tsx
+++ src/components/@UI/Panel/Styled.tsx
@@ -4,17 +4,16 @@ import { Box } from 'theme-ui'
 import colors from '../../../themes/UpshotUI/colors'
 
 interface PanelBaseProps {
-  $inner: boolean,
-  $shadow: string,
+  $inner: boolean
+  $shadow: string
 }
 
 export const PanelBase = styled(Box)<PanelBaseProps>`
-  padding: ${({ theme }) => theme.space[5] + 'px'};
+  padding: 30px;
   background-color: ${({ theme, $inner }) =>
     theme.colors[$inner ? 'black' : 'grey-800']};
   border-radius: ${({ theme, $inner }) => theme.radii[$inner ? 'md' : 'lg']};
   &:hover {
-    ${({ theme, $shadow }) =>
-    $shadow.length > 0 && `box-shadow: ${$shadow};`}
+    ${({ theme, $shadow }) => $shadow.length > 0 && `box-shadow: ${$shadow};`}
   }
 `
