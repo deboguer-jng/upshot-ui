347288adeb48eaefdfcaec71ae23853194611327
DaWe <willteam35@gmail.com>
2022-02-06 21:35:16 +0100

    Adds a new Tooltip component

---
+++ src/components/@UI/Tooltip/Styled.tsx
@@ -0,0 +1,21 @@
+import styled from '@emotion/styled'
+
+import colors from '../../../themes/UpshotUI/colors'
+import Box from '../../Layout/Box'
+import Icon from '../Icon'
+import Panel from '../Panel'
+
+export const BaseBox = styled(Box)`
+  display: inline-block;
+`
+export const ChildContainer = styled(Box)`
+  cursor: pointer;
+`
+export const IconMiddle = styled(Icon)`
+  vertical-align: middle;
+`
+export const StyledPanel = styled(Panel)`
+  backdrop-filter: blur(4px);
+  background-color: rgba(0, 0, 0, 0.6);
+  border-radius: ${({ theme }) => theme.radii.md};
+`
---
+++ src/components/@UI/Tooltip/index.stories.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { ComponentStory, ComponentMeta } from '@storybook/react'
+
+import Tooltip from './'
+import Text from '../Text'
+
+export default {
+  title: '@UI/Tooltip',
+  component: Tooltip,
+} as ComponentMeta<typeof Tooltip>
+
+const Template: ComponentStory<typeof Tooltip> = (args) => (
+  <>
+    <Tooltip {...args} />
+    <Text sx={{ marginTop: '20px', display: 'block' }}>Background</Text>
+  </>
+)
+
+export const Default = Template.bind({})
+Default.args = { tooltip: <Text variant="h3Secondary" sx={{ lineHeight: 1.3 }}>Hello, I'm a tooltip</Text> }
+
