7931f8b3dfd2ec6eb74b390fa78832b7e4db9f00
Ryan Bell <25379378+iRyanBell@users.noreply.github.com>
2022-02-11 11:40:21 -0800

    Improve accessibility

--- src/components/App/Investors/index.stories.tsx
+++ src/components/App/Investors/index.stories.tsx
@@ -7,9 +7,9 @@ export default {
   component: Investors,
 } as ComponentMeta<typeof Investors>
 
-const Template: ComponentStory<typeof Investors> = (args) => <Investors {...args} />
+const Template: ComponentStory<typeof Investors> = (args) => (
+  <Investors {...args} />
+)
 
 export const Default = Template.bind({})
-Default.args = {
-  sx: { width: '100%', height: '100px' },
-}
+Default.args = {}
