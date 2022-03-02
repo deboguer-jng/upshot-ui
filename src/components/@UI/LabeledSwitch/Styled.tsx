4b7d4b2078321a1a5fc28bf634ce5bfdba44558b
Ryan Bell <25379378+iRyanBell@users.noreply.github.com>
2022-03-02 15:59:47 -0800

    Add component

---
+++ src/components/@UI/LabeledSwitch/Styled.tsx
@@ -0,0 +1,28 @@
+import styled from '@emotion/styled'
+
+interface LabeledSwitchButtonProps {
+  $on?: boolean
+}
+
+export const LabeledSwitchBase = styled.div`
+  display: flex;
+  cursor: pointer;
+  width: 100%;
+  border-radius: ${({ theme }) => theme.radii.pill};
+  background: ${({ theme }) => theme.colors.black};
+  box-shadow: ${({ theme }) => theme.rawColors.blue + ' 0px 0px 0px 1.5px'};
+  color: ${({ theme }) => theme.colors.blue};
+`
+
+export const LabeledSwitchButton = styled.div<LabeledSwitchButtonProps>`
+  display: flex;
+  height: 36px;
+  align-items: center;
+  justify-content: center;
+  padding: 0 ${({ theme }) => theme.space[5] + 'px'};
+  background: ${({ theme, $on }) => theme.colors[$on ? 'blue' : 'black']};
+  color: ${({ theme, $on }) => theme.colors[$on ? 'black' : 'blue']};
+  border-radius: ${({ theme }) => theme.radii.pill};
+  transition: ${({ theme }) => theme.transitions.default};
+  flex-grow: ${({ $on }) => ($on ? 0 : 1)};
+`
