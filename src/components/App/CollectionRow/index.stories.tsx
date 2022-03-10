c180893294746ed71c3347981402169d766511ae
Ryan Bell <25379378+iRyanBell@users.noreply.github.com>
2022-03-10 15:53:22 -0800

    Destructure sx props on table

--- src/components/App/CollectionRow/index.stories.tsx
+++ src/components/App/CollectionRow/index.stories.tsx
@@ -77,6 +77,61 @@ const Template = ({ items }: CollectionItems) => {
   )
 }
 
+const TemplateNoImages = ({ items }: CollectionItems) => {
+  const { theme } = useTheme()
+  const breakpointIndex = useBreakpointIndex()
+  const isMobile = breakpointIndex <= 1
+
+  const COLUMNS = ['Column 1', 'Column 2', 'Column 3']
+
+  return (
+    <CollectionTable sx={{ width: 'auto' }}>
+      <TableHead sx={{ color: theme.colors['grey-500'] }}>
+        <TableRow>
+          <TableCell>Name</TableCell>
+          {isMobile ? (
+            // Mobile only shows the first and last columns
+            <TableCell>{`${COLUMNS[0]} & ${COLUMNS.slice(-1)[0]}`}</TableCell>
+          ) : (
+            <>
+              {COLUMNS.map((col, key) => (
+                <TableCell {...{ key }}>{col}</TableCell>
+              ))}
+            </>
+          )}
+        </TableRow>
+      </TableHead>
+
+      <TableBody>
+        {items.map(({ columns, ...args }, key) => (
+          <CollectionRow
+            children={
+              isMobile ? (
+                /* Mobile stacks the first & last columns. */
+                <TableCell sx={{ maxWidth: 100 }}>
+                  <Flex
+                    sx={{ flexDirection: 'column', alignItems: 'flex-end' }}
+                  >
+                    <Flex>{columns[0]}</Flex>
+                    <Flex>{columns.slice(-1)[0]}</Flex>
+                  </Flex>
+                </TableCell>
+              ) : (
+                COLUMNS.map((column, key) => (
+                  <TableCell sx={{ maxWidth: 100 }} {...{ key }}>
+                    {column}
+                  </TableCell>
+                ))
+              )
+            }
+            {...{ key, ...args }}
+          />
+        ))}
+      </TableBody>
+    </CollectionTable>
+  )
+}
+
 export const Default = Template.bind({})
 Default.args = {
   items: [
@@ -98,6 +153,27 @@ Default.args = {
     {
       title: 'CryptoPunk #1025',
       imageSrc: '/img/defaultAvatar.png',
+      columns: ['Value 1', 'Value 2', 'Value 3'],
+    },
+  ],
+}
+
+export const NoImages = TemplateNoImages.bind({})
+NoImages.args = {
+  items: [
+    {
+      title:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum sodales quis ex nec sagittis.',
+      columns: ['Value 1', 'Value 2', 'Value 3'],
+      pixelated: true,
+    },
+    {
+      title:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum sodales quis ex nec sagittis.',
+      columns: ['Value 1', 'Value 2', 'Value 3'],
+    },
+    {
+      title: 'CryptoPunk #1025',
       columns: ['Value 1', 'Value 2', 'Value 3'],
     },
   ],
