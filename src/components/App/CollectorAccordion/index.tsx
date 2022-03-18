3a91f787072842930b2eef7a3ab0dc2121b259c9
Ryan Bell <25379378+iRyanBell@users.noreply.github.com>
2022-03-18 11:23:10 -0700

    Prop for full width expansion on rows

--- src/components/App/CollectorAccordion/index.tsx
+++ src/components/App/CollectorAccordion/index.tsx
@@ -1,17 +1,29 @@
 import React, { forwardRef } from 'react'
 import { Flex, FlexProps } from 'theme-ui'
 
-export interface CollectorAccordionProps extends FlexProps {}
+export interface CollectorAccordionProps extends FlexProps {
+  /**
+   * Expand to 100% of the container width.
+   */
+  fullWidth?: boolean
+}
 
 /**
  * Provides an accordion wrapper for the collectors table.
  */
 const CollectorAccordion = forwardRef(
   (
-    { children, ...props }: CollectorAccordionProps,
+    { children, fullWidth, ...props }: CollectorAccordionProps,
     ref: React.ForwardedRef<HTMLDivElement>
   ) => (
-    <Flex {...{ ref, ...props }} sx={{ flexDirection: 'column', gap: 4 }}>
+    <Flex
+      {...{ ref, ...props }}
+      sx={{
+        flexDirection: 'column',
+        gap: 4,
+        width: fullWidth ? '100%' : 'auto',
+      }}
+    >
       {children}
     </Flex>
   )
