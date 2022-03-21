bd6af75e24e1cbd3043ed025fd1e58d316b056e0
Ryan Bell <25379378+iRyanBell@users.noreply.github.com>
2022-03-21 14:59:49 -0700

    Format nft count

--- src/components/App/CollectionRow/index.tsx
+++ src/components/App/CollectionRow/index.tsx
@@ -218,7 +218,7 @@ const CollectionRow = forwardRef(
                         lineHeight: 1,
                       }}
                     >
-                      {nftCount}
+                      {formatCommas(nftCount)}
                     </Text>
                   </Flex>
                 </>
