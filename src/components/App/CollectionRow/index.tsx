267d9d73e22eb765a5233e4c9cdabfd3ec9c3c64
Ryan Bell <25379378+iRyanBell@users.noreply.github.com>
2022-03-21 15:00:14 -0700

    Import util

--- src/components/App/CollectionRow/index.tsx
+++ src/components/App/CollectionRow/index.tsx
@@ -13,6 +13,7 @@ import { TableRowProps } from '../../Layout/TableRow'
 import { Icon, useBreakpointIndex } from '../../..'
 import { Flex, IconButton } from '@theme-ui/components'
 import { imageOptimizer } from '../../../utils/imageOptimizer'
+import { formatCommas } from '../../../utils/number'
 import { useTheme } from '@emotion/react'
 export type Variant = 'black' | 'dark' | 'normal'
 export interface CollectionRowProps extends TableRowProps {
