27060a10aea3f09655ec940844298a616ee0c000
DaWe <willteam35@gmail.com>
2022-03-22 13:11:23 +0100

    Revert "add line-height: initial;"
    This reverts commit 05f897b109b6a94eb22cdaf19185a10c7a027baa.

--- src/components/@UI/Link/Styled.tsx
+++ src/components/@UI/Link/Styled.tsx
@@ -4,7 +4,6 @@ import { Link } from 'theme-ui'
 export const StyledLink = styled(Link)`
   color: #FFF;
   text-decoration: none;
-  line-height: initial;
   :hover {
     text-decoration: underline;
   }
