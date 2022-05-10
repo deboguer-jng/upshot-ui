6eb15188c980c99654fee6b1ef0e54511f588eb2
Ryan Bell <25379378+iRyanBell@users.noreply.github.com>
2022-05-10 15:02:13 -0700

    Opt for % of parent instead of viewport

--- src/components/@UI/Modal/Styled.tsx
+++ src/components/@UI/Modal/Styled.tsx
@@ -12,7 +12,7 @@ export const ModalWrapper = styled.div<ModalWrapperProps>`
   left: 50%;
   max-height: 100vh;
   overflow-y: auto;
-  width: ${({ $fullWidth }) => ($fullWidth ? '100vw' : 'auto')};
+  width: ${({ $fullWidth }) => ($fullWidth ? '100%' : 'auto')};
   transform: translate(-50%, -50%);
   z-index: ${({ theme }) => theme.zIndex.modal};
   animation: ${keyframes`from { opacity: 0; }`} 0.25s ease;
