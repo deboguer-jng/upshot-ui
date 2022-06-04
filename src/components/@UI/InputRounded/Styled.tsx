ca7cf3cf27c163e976a7b0b98f21e6b9f6fcf977
jayknapp <jay@upshot.io>
2022-06-03 19:25:44 -0500

    add prefix, suffix props

--- src/components/@UI/InputRounded/Styled.tsx
+++ src/components/@UI/InputRounded/Styled.tsx
@@ -1,8 +1,45 @@
 import styled from '@emotion/styled'
-import { Input } from 'theme-ui'
+import { Flex, Input, Text } from 'theme-ui'
+import theme from '../../../themes/UpshotUI'
+import sizes, { getSize } from '../../../themes/UpshotUI/sizes'
 
-export const WrappedInput = styled(Input)`
+interface WrappedInputProps {
+  $suffix: string
+}
+
+interface ContainerProps {
+  $dark: boolean
+}
+
+interface SuffixTextProps {
+  $disabled: boolean
+}
+
+export const WrappedInputContainer = styled(Flex)<ContainerProps>`
+  display: inline-flex;
+  height: ${getSize(10) + 'px'};
+  background-color: ${({ $dark }) => $dark ? 'black': theme.colors['grey-800']};
+  border-radius: ${theme.radii.pill};
+`
+
+export const WrappedInput = styled(Input)<WrappedInputProps>`
   &:focus::placeholder {
     color: transparent;
   }
+  /* Leave space for the right-aligned suffix. */
+  ${({ theme, $suffix }) =>
+    $suffix?.length &&
+    `padding-right: ${theme.forms.inputs.default.height};`
+  }
+`
+
+export const SuffixText = styled(Text)<SuffixTextProps>`
+  padding: ${sizes[3] + 'px'};
+  ${({theme, $disabled}) => $disabled && `color: ${theme.colors['grey-500']};`}
+`
+
+export const PrefixText = styled(Text)<SuffixTextProps>`
+  padding: ${sizes[3] + 'px'};
+  padding-right: 0;
+  ${({theme, $disabled}) => $disabled && `color: ${theme.colors['grey-500']};`}
 `
--- src/components/@UI/InputRounded/index.stories.tsx
+++ src/components/@UI/InputRounded/index.stories.tsx
@@ -17,3 +17,15 @@ Default.args = { placeholder: 'Search...', sx: { width: 300 } }
 
 export const Dark = Template.bind({})
 Dark.args = { dark: true, placeholder: 'Search...', sx: { width: 300 } }
+
+export const WithSuffix = Template.bind({})
+WithSuffix.args = { placeholder: '100', suffix: '%', sx: { width: 300 } }
+
+export const WithSuffixDisabled = Template.bind({})
+WithSuffixDisabled.args = { placeholder: '100', suffix: '%', disabled: true, sx: { width: 300 } }
+
+export const WithPrefix = Template.bind({})
+WithPrefix.args = { placeholder: '100', prefix: '$', sx: { width: 300 } }
+
+export const WithPrefixAndSuffix = Template.bind({})
+WithPrefixAndSuffix.args = { placeholder: '100', prefix: '+/-', suffix: '%', sx: { width: 300 } }
